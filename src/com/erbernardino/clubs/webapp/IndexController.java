365f5f1077aafa1fdd2d35aecb18b796b6817d9d
Emerson Rocco Bernardino <emerson.rocco@gmail.com>
2010-12-14 17:21:37 -0200

    Create Project

---
+++ src/com/erbernardino/clubs/webapp/IndexController.java
@@ -0,0 +1,13 @@
+package com.erbernardino.clubs.webapp;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class IndexController {
+
+	@Path("/")
+	public void index() {
+	}
+	
+}
